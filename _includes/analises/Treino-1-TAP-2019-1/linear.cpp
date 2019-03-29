fd10ed47f35e978077de071a7108c844f1b69a5e
Welton Cardoso do Carmo <wellvolks@hotmail.com>
2019-03-29 00:47:33 -0300

    Create linear.cpp

---
+++ _includes/analises/Treino-1-TAP-2019-1/linear.cpp
@@ -0,0 +1,14 @@
+ <div id="linear">
+ 
+ </div>
+## P6 - Linear Cellular Automata
+
+Resolva este problema:
+[UVA][uva]{:target="_blank"}
+
+
+Um exemplo de implementação segue abaixo:
+
+{% gist wellvolks/cab1c3c3dd8c4673d0c97b6ca417fa69 linear.cppp %}
+
+[uva]:		https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=398
